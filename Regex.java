3c4e05e91b7a746e5ef1552cdce7a11e48aedcb2
codeauslander <cesar.gusto.catano@gmail.com>
2018-11-22 00:06:56 -0600

    Regex expression to match IP addresses

---
+++ Regex.java
@@ -0,0 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex {
+    /*
+     * [01]?\\d{1,2} matches numbers 0-199
+     * 2[0-4]?\\d matches numbers 200-249
+     * 25[0-5] matches number 250-255
+     */
+
+    String number = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    String pattern = number + "." + number + "." + number + "." + number;
+
+    
+    
+}
