2cdda17d9c4c8bccfbaac90d0ce0b6cbbd2f085a
codeauslander <cesar.gusto.catano@gmail.com>
2018-12-19 23:18:03 -0600

    End of Strings exercises at HackerRank

--- HackerRank/StringTokens.java
+++ HackerRank/StringTokens.java
@@ -1,3 +1,4 @@
+// https://www.hackerrank.com/challenges/java-string-tokens/problem
 import java.io.*;
 import java.util.*;
 
@@ -5,14 +6,17 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        System.out.println(s);
-        String[] tokens = s.split("[A-Za-z !,?._'@]+");
+        String s = scan.nextLine().trim();
+        scan.close();
+        if( s.length() == 0){
+            System.out.println(s.length());
+            return;
+        }
+        String[] tokens = s.split("[^A-Za-z]+");
         System.out.println(tokens.length);
         for(String token : tokens){
             System.out.println(token);
         }
-        scan.close();
     }
 }
 
